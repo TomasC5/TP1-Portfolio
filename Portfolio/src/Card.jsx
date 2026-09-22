adfafb79b6cbc6f63e4f4cb953e349b9556a5528
TomasC5 <tomas.azumas@gmail.com>
2026-09-22 14:05:30 -0300

    estilizacion de card

--- Portfolio/src/Card.jsx
+++ Portfolio/src/Card.jsx
@@ -1,11 +1,11 @@
-
+import pfp from './assets/pfp.jpg'
 
 function Card() {
     return(
         <div className = "card">
-            <img src= "https://via.placeholder.com/150" alt="pfp"></img>
-            <h2>Tomás Azubel</h2>
-            <p>Programming with React + Vite</p>
+            <img className="card-image" src={pfp} alt="pfp" width="150"></img>
+            <h2 className="card-title">Tomás Azubel</h2>
+            <p className="card-text">Programming with React + Vite</p>
         </div>
     );
 }
