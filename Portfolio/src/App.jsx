d80841c1f7b8f345ec1927021bfe48b5bf6f8245
TomasC5 <tomas.azumas@gmail.com>
2026-09-22 13:50:58 -0300

    creacion de card

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Skills from "./Skills.jsx"
+import Card from "./Card.jsx"
 
 function App() {
 
     return(
       <>
         <Header/>
+        <Card/>
         <Skills/>
         <Footer/>
       </>
---
+++ Portfolio/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card() {
+    return(
+        <div className = "card">
+            <img src= "https://via.placeholder.com/150" alt="pfp"></img>
+            <h2>Tomás Azubel</h2>
+            <p>Programming with React + Vite</p>
+        </div>
+    );
+}
+
+export default Card
