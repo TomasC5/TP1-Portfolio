7593c2b82a87671c56e54090153bc4cfd5251d15
TomasC5 <tomas.azumas@gmail.com>
2026-09-22 20:56:04 -0300

    creacion de listas (temporal)

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,8 +4,14 @@ import Skills from "./Skills.jsx"
 import Card from "./Card.jsx"
 import Button from "./Button/Button.jsx"
 import UserGreeting from "./UserGreeting.jsx"
+import List from "./List.jsx"
 
 function App() {
+
+  const classesTaken = [{id: 1, nameOfClass: "programming IV", amount: 12},
+                        {id: 2, nameOfClass: "programming III", amount: 24},
+                        {id: 3, nameOfClass: "programming II", amount: 27},
+                        {id: 4, nameOfClass: "programming I", amount: 8}];
 
     return(
       <>
@@ -15,6 +21,7 @@ function App() {
         <Card/>
         <Button/>
         <Skills/>
+        <List classes={classesTaken} category="Programming"/>
         <Footer/>
       </>
     );
---
+++ Portfolio/src/List.jsx
@@ -0,0 +1,16 @@
+function List(props) {
+
+    const category = props.category;
+    const classList = props.classes;
+
+    const listClasses = classList.map(progClass => <li key ={progClass.id}>
+                                                <b>{progClass.nameOfClass}:</b> &nbsp;
+                                                {progClass.amount}</li>);
+
+    return(<>
+    <h3>{category}</h3>
+    <ul>{listClasses}</ul>
+    </>);
+}
+
+export default List
